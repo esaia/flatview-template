c01bc314792fa4498efa8b665716923e4905f95d
Esaia <esaiagafrindashvili@gmail.com>
2026-06-15 16:20:15 +0400

    chore: update Home page

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -8,7 +8,10 @@ defineProps({
     <div class="min-h-screen bg-gray-50 flex items-center justify-center">
         <div class="text-center">
             <h1 class="text-4xl font-bold text-gray-900 mb-4">{{ appName }}</h1>
-            <p class="text-gray-500">Laravel + Inertia + Vue + Tailwind + Vite</p>
+            <p class="text-gray-500">
+                Laravel + Inertia + Vue + Tailwind + Vite
+            </p>
+            123123
         </div>
     </div>
 </template>
